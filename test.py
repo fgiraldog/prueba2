a694ba9999b6751dd0776a5b8e2a9c7303e8ba82
fgiraldog <f.giraldo@uniandes.edu.co>
2018-11-21 18:11:58 +0000

    Prueba

--- test.py
+++ test.py
@@ -5,3 +5,14 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 print("hola mundo cruel")
+
+def parabola(x):
+        
+            return x*x
+       
+    
+x = np.linspace(0,1,100)
+
+plt.figure()
+plt.plot(x,parabola(x))
+plt.show()
